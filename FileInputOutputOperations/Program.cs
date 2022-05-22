84781602c4b058f89c1dae4104214fef7a085306
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-22 17:25:42 +0530

    [Harshal]Add:File Input Output Operations

---
+++ FileInputOutputOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FileInputOutputOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------Welcome--------------");
+            FileOperations operations = new FileOperations();
+            operations.FileExist();
+            operations.ReadAllLines();
+            operations.ReadAllText();
+            operations.CopyFile();
+            operations.DeleteFilePath();
+            operations.UsingStreamReader();
+        }
+        
+    }
+
+}
